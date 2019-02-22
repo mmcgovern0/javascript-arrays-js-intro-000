6bf92bd07e348ce05f6e4383a9c78b08c1d865db
Michael McGovern <mmcgovern.wework@gmail.com>
2019-02-22 20:39:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
 }
 
 function removeElementFromBeginningOfArray(chocolateBars){
-  chocolateBars.slice(1);
+  chocolateBars.slice(0);
 }
 
 function destructivelyRemoveElementFromEndOfArray(chocolateBars){
